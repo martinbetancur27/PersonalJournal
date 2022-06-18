eef004377892e50220cd6783c7d25e40418de79f
Martin Betancur <martinbetancur27@gmail.com>
2022-06-18 18:47:56 -0500

    create project and layers

---
+++ VirtualJournalMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VirtualJournalMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
