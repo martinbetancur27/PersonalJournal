c4ac9ba85f28b9964b3c1d7b3f289d894e9c7829
Martin Betancur <martinbetancur27@gmail.com>
2022-06-18 21:45:31 -0500

    DbContext -> Add models and change the tables name

--- Data/VirtualJournalDbContext.cs
+++ Data/VirtualJournalDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace Data
 {
@@ -12,6 +13,32 @@ namespace Data
         public VirtualJournalDbContext(DbContextOptions<VirtualJournalDbContext> options) : base(options)
         {
 
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<People> People { get; set; }
+
+        public DbSet<ClientWriter> ClientWriters { get; set; }
+
+        public DbSet<Journal> Journals { get; set; }
+
+        public DbSet<Note> Notes { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Administrator> Administrators { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Account>().ToTable("Account");
+            modelbuilder.Entity<People>().ToTable("People");
+            modelbuilder.Entity<ClientWriter>().ToTable("ClientWriter");
+            modelbuilder.Entity<Journal>().ToTable("Journal");
+            modelbuilder.Entity<Note>().ToTable("Note");
+            modelbuilder.Entity<Comment>().ToTable("Comment");
+            modelbuilder.Entity<Administrator>().ToTable("Administrator");
         }
     }
 }
