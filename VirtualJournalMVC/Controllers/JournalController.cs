d76a051f9a355a29d62cc1922241cebd52acc7aa
Martin Betancur <martinbetancur27@gmail.com>
2022-06-24 16:31:20 -0500

    Add controller and views (without programming)

---
+++ VirtualJournalMVC/Controllers/JournalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualJournalMVC.Controllers
+{
+    public class JournalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
