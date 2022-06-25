9d7bb24eeccf9cf414aee6bfbaa49f8774843aa0
Martin Betancur <martinbetancur27@gmail.com>
2022-06-24 20:41:43 -0500

    Creation of the html structure for all the pages -> Use of Bootstrap

--- VirtualJournalMVC/Controllers/JournalController.cs
+++ VirtualJournalMVC/Controllers/JournalController.cs
@@ -8,5 +8,51 @@ namespace VirtualJournalMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult AddJournal()
+        {
+            return View();
+        }
+
+        public IActionResult DetailsJournal()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateJournal()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteJournal()
+        {
+            return View();
+        }
+
+        public IActionResult Notes()
+        {
+            return View();
+        }
+
+        public IActionResult ShowNote()
+        {
+            return View();
+        }
+
+        public IActionResult AddNote()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateNote()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteNote()
+        {
+            return View();
+        }
+
     }
 }
