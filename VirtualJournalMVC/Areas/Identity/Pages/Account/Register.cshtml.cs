877080bc5233df2477d0cc0eea239db616584062
Martin Betancur <martinbetancur27@gmail.com>
2022-11-16 16:47:49 -0500

    Configuration to deploy application in Azure as AppService and Azure Sql Server + Fixed: redirect to Journal when registering a user

--- VirtualJournalMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VirtualJournalMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,7 +109,7 @@ namespace VirtualJournalMVC.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Journal");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
--- VirtualJournalMVC/Program.cs
+++ VirtualJournalMVC/Program.cs
@@ -8,10 +8,22 @@ using Data.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add DbContext
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("VirtualJournalConnection")
-    ));
+
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("VirtualJournalConnectionProduction")
+        ));
+}
+else
+{
+    // Add DbContext
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("VirtualJournalConnection")
+        ));
+}
+
+builder.Services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
 
 
 // Add Services
@@ -83,6 +95,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
